96db9ec29db87788b642b4c81343c53e88a8ea8f
ElninoFong <ElninoFong@me.com>
2014-02-17 22:33:50 -0500

    2.4 done

---
+++ 2.4 partition.java
@@ -0,0 +1,45 @@
+2.4 Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
+
+// use four pointers to mark the start and end of two parts(greater and less)
+// TC: O(n), SC: O(n)
+LinkedListNode partition(LinkedListNode head, int x) {
+	if (head == null) return null;
+	LinkedListNode frontStart = null;
+	LinkedListNode frontEnd = null;
+	LinkedListNode behindStart = null;
+	LinkedListNode behindEnd = null;
+	while (head != null) {
+		if (head.data < x) {
+			if (frontStart == null) {
+				frontStart = head;
+			}
+			if (frontEnd == null) {
+				frontEnd = head;
+			} else {
+				frontEnd.next = head;
+				frontEnd = head;
+			}
+		} else {
+			if (behindStart == null) {
+				behindStart = head;
+			}
+			if (behindEnd == null) {
+				behindEnd = head;
+			} else {
+				behindEnd.next = head;
+				behindEnd = head;
+			}
+		}
+		head = head.next;
+	}
+	// connect two parts
+	if (frontEnd == null) {
+		return behindStart;
+	}
+	frontEnd.next = behindStart;
+	if (behindEnd != null) {
+		behindEnd.next = null;
+	}
+
+	return frontStart;
+}
